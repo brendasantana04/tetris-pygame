25aeb6a1b532806b9f84a73540857bbfa551caed
brendasantana04 <dollanganger.8@gmail.com>
2025-01-05 23:33:33 -0300

    Iniciando projeto de TETRIS

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pygame,sys
+import pygame
+import sys
+from grid import Grid
 
 pygame.init()
 dark_blue = (44, 44, 127)
@@ -9,6 +11,11 @@ pygame.display.set_caption("TETRIS")
 
 clock = pygame.time.Clock()
 
+# definindo uma grid para o jogo
+game_grid = Grid()
+game_grid.print_grid()
+
+
 # loop para manter a tela aberta
 while True:
     for event in pygame.event.get():
@@ -18,6 +25,7 @@ while True:
 
     # desenho
     screen.fill(dark_blue)
+    game_grid.draw(screen)
 
     pygame.display.update()
     clock.tick(60) # 60 frames por segundo
